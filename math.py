9bac0f856c77b277d9533882692cbb006dfd87b4
pandakkkk <sanjeev.murmu.mat17@itbhu.ac.in>
2021-06-16 11:59:30 +0530

    Implemented Multiplication on Bug456 branch

--- math.py
+++ math.py
@@ -6,7 +6,7 @@ def subtract(x,y):
     return x-y  #on master branch
 #multiplication implementation
 def multiply(x,y):
-    pass
+    return x*y  # in Bug456 branch
 #division implementation
 def divide(x,y):
     pass
